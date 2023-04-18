f49af55649335e96e7b421c3832a0fd02df44f43
Tate Reynolds <tate.reynolds@gmail.com>
2023-04-18 12:42:24 -0600

    comments

--- attack3.py
+++ attack3.py
@@ -5,8 +5,10 @@ import sys
 import hashlib
 from scapy.all import *
 
+# Disable scapy warnings
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
+# Extract the file name from the FTP RETR command in the packet data
 def extract_file_name(data):
     regex = r"RETR (.+)"
     match = re.search(regex, data)
@@ -14,6 +16,7 @@ def extract_file_name(data):
         return match.group(1).strip().lower()
     return None
 
+# Extract the IP address and port for the passive FTP data connection
 def extract_pasv_ip_port(data):
     regex = r"Entering Passive Mode \((\d+,\d+,\d+,\d+),(\d+),(\d+)\)"
     match = re.search(regex, data)
@@ -23,28 +26,34 @@ def extract_pasv_ip_port(data):
         return ip, port
     return None, None
 
+# Process the packets to extract file name, passive connection IP and port, and file content
 def parsePacket(packet, file_data):
     if not packet.haslayer("TCP"):
         return None
 
     data = bytes(packet["TCP"].payload).decode('utf-8', 'replace')
 
+    # Extract the file name if it hasn't been found yet
     if not file_data["file_name"]:
         file_name = extract_file_name(data)
         if file_name:
             file_data["file_name"] = file_name
 
+    # Extract the passive IP and port if they haven't been found yet
     if not file_data["pasv_ip"] or not file_data["pasv_port"]:
         pasv_ip, pasv_port = extract_pasv_ip_port(data)
         if pasv_ip and pasv_port:
             file_data["pasv_ip"] = pasv_ip
             file_data["pasv_port"] = pasv_port
 
+    # Collect the file content if all necessary information has been found
     if packet.haslayer("Raw") and file_data["file_name"] and file_data["pasv_ip"] and file_data["pasv_port"]:
         if (packet["IP"].src == file_data["pasv_ip"] and packet["TCP"].sport == file_data["pasv_port"]) or (packet["IP"].dst == file_data["pasv_ip"] and packet["TCP"].dport == file_data["pasv_port"]):
             file_data["file_content"].append(packet["Raw"].load)
 
+# Main function
 if __name__ == "__main__":
+    # Initialize the file data dictionary
     file_data = {
         "file_name": None,
         "file_content": [],
@@ -52,9 +61,11 @@ if __name__ == "__main__":
         "pasv_port": None,
     }
 
+    # Iterate through the packets in the pcap file and process them
     for packet in rdpcap(sys.argv[1]):
         parsePacket(packet, file_data)
 
+    # Save the extracted file if a file name has been found
     if file_data["file_name"]:
         with open(file_data["file_name"], "wb") as f:
             for content in file_data["file_content"]:
